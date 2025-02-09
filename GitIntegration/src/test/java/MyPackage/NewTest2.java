529b2eb81004b00568ec17ade1889297bc782b7e
swami <swami@LAPTOP-E84GLKHG>
2025-02-09 14:32:25 -0800

    Update Project config

---
+++ GitIntegration/src/test/java/MyPackage/NewTest2.java
@@ -0,0 +1,19 @@
+package MyPackage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
---
+++ GitIntegration/src/test/java/MyPackage/Random.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class Random {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello");
+	}
+
+}
